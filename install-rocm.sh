402a195d24fab9d7acb1291b21f8f8c5f32c985b
Sean McKeown <seangcxq@gmail.com>
2018-03-07 06:44:21 -0800

    initial commit

---
+++ install-rocm.sh
@@ -0,0 +1,5 @@
+wget -qO - http://repo.radeon.com/rocm/apt/debian/rocm.gpg.key | sudo apt-key add -
+sudo sh -c 'echo deb [arch=amd64] http://repo.radeon.com/rocm/apt/debian/ xenial main > /etc/apt/sources.list.d/rocm.list'
+sudo apt-get update
+sudo apt-get install rocm
+sudo reboot
